5570dd367507d224898886d6318244ed3be863e2
liguanghao <chiansoft_lgh@outlook.com>
2017-03-29 13:42:03 +0800

    framework

--- src/main/java/com/lgh/spring/boot/controller/HomeController.java
+++ src/main/java/com/lgh/spring/boot/controller/HomeController.java
@@ -2,21 +2,22 @@ package com.lgh.spring.boot.controller;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by Administrator on 2017/3/1.
  */
-@RestController
-@RequestMapping("/home")
+@Controller
+@RequestMapping(value = "/")
 @Api(tags = "home")
 public class HomeController {
-    @ApiOperation(value = "test",notes = "test")
-    @PostMapping(value = "/test",name = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public String test(){
-        return "this is a test demo";
+    @ApiOperation(value = "index",notes = "index")
+    @GetMapping(value = "/index")
+    public String test(Model model){
+        model.addAttribute("name","index");
+        return "index";
     }
 }
