b9efb36ffc9b1e36bdf50b82291828262a04b6b5
liguanghao <chiansoft_lgh@outlook.com>
2018-06-23 08:58:56 +0800

    开发者-代码仓库

--- src/main/java/com/lgh/spring/boot/Application.java
+++ src/main/java/com/lgh/spring/boot/Application.java
@@ -1,7 +1,9 @@
 package com.lgh.spring.boot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -10,6 +12,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
  * @author Administrator
  */
 @SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
+@EnableAutoConfiguration(exclude = {SecurityAutoConfiguration.class})
 public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
