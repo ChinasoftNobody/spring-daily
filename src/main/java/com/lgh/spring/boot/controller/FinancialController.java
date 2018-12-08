5c2443b9cedd3c3e18bdf2609455b2d018242470
maysham <chinasoft_lgh@outlook.com>
2018-12-08 22:54:15 +0800

    收支记录

--- src/main/java/com/lgh/spring/boot/controller/FinancialController.java
+++ src/main/java/com/lgh/spring/boot/controller/FinancialController.java
@@ -2,7 +2,9 @@ package com.lgh.spring.boot.controller;
 
 import com.github.pagehelper.Page;
 import com.lgh.spring.boot.model.MRecord;
+import com.lgh.spring.boot.model.MUser;
 import com.lgh.spring.boot.service.financial.FinancialService;
+import com.lgh.spring.boot.service.login.UserService;
 import com.lgh.spring.boot.util.UiPath;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * 财务管理
@@ -19,11 +22,15 @@ import javax.annotation.Resource;
 public class FinancialController {
     @Resource
     private FinancialService financialService;
+    @Resource
+    private UserService userService;
 
     @GetMapping("")
     public String index(Model model){
         Page<MRecord> recordPage = financialService.queryAll();
+        List<MUser> users = userService.queryAll();
         model.addAttribute("recordPage", recordPage);
-        return UiPath.setPath(model, "/finance/index");
+        model.addAttribute("users", users);
+        return UiPath.setPath(model, "/finance/index",null,"/static/js/financial/index.js");
     }
 }
---
+++ src/main/resources/static/js/financial/index.js
@@ -0,0 +1,9 @@
+var FinancialService = {
+    chooseOccurrenceMember:function(id,name){
+        $('#occurrenceMemberId').val(id);
+        $('#occurrenceMemberName').val(name);
+    },
+    createRecord: function () {
+        var form = $('#CreateRecordForm');
+    }
+};
