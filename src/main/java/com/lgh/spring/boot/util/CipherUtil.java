7a0faeaffc153b42edd5da0dab130998b2043beb
chinasoft_lgh@outlook.com <vs6531474>
2018-12-11 00:17:39 +0800

    1

--- src/main/java/com/lgh/spring/boot/util/CipherUtil.java
+++ src/main/java/com/lgh/spring/boot/util/CipherUtil.java
@@ -1,87 +1,15 @@
 package com.lgh.spring.boot.util;
 
-import sun.misc.BASE64Decoder;
-import sun.misc.BASE64Encoder;
-
-import javax.crypto.Cipher;
-import javax.crypto.SecretKey;
-import javax.crypto.SecretKeyFactory;
-import javax.crypto.spec.DESKeySpec;
-import java.io.UnsupportedEncodingException;
-import java.security.SecureRandom;
+import java.nio.charset.Charset;
+import java.util.Base64;
 
 public class CipherUtil {
 
-    private static final String desPassword = "9588028820109132570743325311898426347857298773549468758875018579537757772163084478873699447306034466200616411960574122434059469100235892702736860872901247123456";
-
-    public static String desEncode(String target) {
-        try {
-            SecureRandom random = new SecureRandom();
-            DESKeySpec desKey = new DESKeySpec(desPassword.getBytes());
-            //创建一个密匙工厂，然后用它把DESKeySpec转换成
-            SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("DES");
-            SecretKey securekey = keyFactory.generateSecret(desKey);
-            //Cipher对象实际完成加密操作
-            Cipher cipher = Cipher.getInstance("DES");
-            //用密匙初始化Cipher对象
-            cipher.init(Cipher.ENCRYPT_MODE, securekey, random);
-            //现在，获取数据并加密
-            //正式执行加密操作
-            return new String(cipher.doFinal(target.getBytes()));
-        } catch (Throwable e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    public static String desDecode(String target) {
-        try {
-            // DES算法要求有一个可信任的随机数源
-            SecureRandom random = new SecureRandom();
-            // 创建一个DESKeySpec对象
-            DESKeySpec desKey = new DESKeySpec(desPassword.getBytes());
-            // 创建一个密匙工厂
-            SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("DES");
-            // 将DESKeySpec对象转换成SecretKey对象
-            SecretKey securekey = keyFactory.generateSecret(desKey);
-            // Cipher对象实际完成解密操作
-            Cipher cipher = Cipher.getInstance("DES");
-            // 用密匙初始化Cipher对象
-            cipher.init(Cipher.DECRYPT_MODE, securekey, random);
-            // 真正开始解密操作
-            return new String(cipher.doFinal(target.getBytes()));
-        } catch (Exception e){
-            e.printStackTrace();
-        }
-        return null;
-    }
-
     public static String base64Encode(String target){
-        byte[] b = null;
-        String s = null;
-        try {
-            b = target.getBytes("utf-8");
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
-        if (b != null) {
-            s = new BASE64Encoder().encode(b).replace("\r\n","");
-        }
-        return s;
+        return Base64.getEncoder().encodeToString(target.getBytes(Charset.forName("UTF-8")));
     }
 
     public static String base64Decode(String target) {
-        byte[] b = null;
-        String result = null;
-        if (target != null) {
-            BASE64Decoder decoder = new BASE64Decoder();
-            try {
-                b = decoder.decodeBuffer(target);
-                result = new String(b, "utf-8");
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-        return result;
+        return new String(Base64.getDecoder().decode(target.getBytes(Charset.forName("UTF-8"))));
     }
 }
