ba66c128def479dfc95777f49891de5d273ef40e
chinasoft_lgh@outlook.com <vs6531474>
2018-12-18 22:38:22 +0800

    jlayer

--- src/main/java/com/lgh/spring/boot/service/clock/impl/VoiceClockServiceImpl.java
+++ src/main/java/com/lgh/spring/boot/service/clock/impl/VoiceClockServiceImpl.java
@@ -1,58 +1,32 @@
 package com.lgh.spring.boot.service.clock.impl;
 
 import com.lgh.spring.boot.model.MVoiceClockMessage;
-import com.lgh.spring.boot.service.clock.Sound;
 import com.lgh.spring.boot.service.clock.VoiceClockService;
-import javafx.application.Application;
-import javafx.stage.Stage;
+import com.lgh.spring.boot.util.JLUtil;
 import org.springframework.stereotype.Service;
 
-import java.net.URLEncoder;
 import java.util.concurrent.ArrayBlockingQueue;
 
 @Service
-public class VoiceClockServiceImpl extends Application implements VoiceClockService {
+public class VoiceClockServiceImpl implements VoiceClockService {
     private static ArrayBlockingQueue<MVoiceClockMessage> messageBlockingQueue = new ArrayBlockingQueue<MVoiceClockMessage>(10);
-    private static final String SPEAK_PATH = "https://gss0.baidu.com/6KAZsjip0QIZ8tyhnq/text2audio";
 
     @Override
     public void start() {
         try {
             new Thread(() -> {
                 try {
-                    launch(VoiceClockServiceImpl.class);
+                    while (true) {
+                        MVoiceClockMessage mVoiceClockMessage = messageBlockingQueue.take();
+                        JLUtil.play(mVoiceClockMessage);
+                    }
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
             }).start();
-            System.out.println(1);
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }
-
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        //if queue is empty, this will wait
-        new Thread(() -> {
-            try {
-                while (true) {
-                    MVoiceClockMessage mVoiceClockMessage = messageBlockingQueue.take();
-                    String url = "?tex=" + URLEncoder.encode(mVoiceClockMessage.getVolTex(),"utf-8")
-                            + "&cuid=" + mVoiceClockMessage.getVolCuid()
-                            + "&lan=" + mVoiceClockMessage.getVolLan()
-                            + "&ctp=" + mVoiceClockMessage.getVolCtp()
-                            + "&pdt=" + mVoiceClockMessage.getVolPdt()
-                            + "&vol=" + mVoiceClockMessage.getVolVol()
-                            + "&spd=" + mVoiceClockMessage.getVolSpd();
-                    Sound sound = new Sound(SPEAK_PATH + url, false);
-                    sound.play();
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }).start();
-        System.out.println(2);
     }
 
     @Override
